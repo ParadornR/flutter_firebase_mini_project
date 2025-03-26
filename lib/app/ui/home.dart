dc0f8de451336cb6cf127a301031bf8b15304fd7
imxintro5 <paradorn.rinrit63@gmail.com>
2025-03-26 15:16:56 +0700

    [mix]"config home for use firebase"

--- lib/app/ui/home.dart
+++ lib/app/ui/home.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_mini_project/app/ui/add_menu.dart';
 
 class Home extends StatelessWidget {
- Home({super.key});
-   // collection users
-  final users = FirebaseFirestore.instance.collection('users');
+  Home({super.key});
+  // collection menu
+  final menu = FirebaseFirestore.instance.collection('foodMenu');
+
+  // real collection
+  Stream<QuerySnapshot> getMenu() {
+    return menu.snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,55 +22,79 @@ class Home extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.separated(
-          itemCount: 2,
-          separatorBuilder: (context, index) => SizedBox(height: 8),
-          itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: MediaQuery.of(context).size.height * 0.2,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('chef data', style: TextStyle(fontSize: 14)),
-                        Text(
-                          'menu data',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: getMenu(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final docs = snapshot.data!.docs;
+            return ListView.separated(
+              itemCount: docs.length,
+              separatorBuilder: (context, index) => SizedBox(height: 8),
+              itemBuilder: (context, index) {
+                final data = docs[index].data() as Map<String, dynamic>;
+                return Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(data["chef"], style: TextStyle(fontSize: 14)),
+                            Text(
+                              data["name"],
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              data["ingredients"],
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ],
                         ),
-                        Text('info data', style: TextStyle(fontSize: 12)),
-                      ],
-                    ),
+                      ),
+                      Expanded(
+                        child: Image.network(
+                          '',
+                          fit: BoxFit.cover,
+                          loadingBuilder: (context, child, loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return CircularProgressIndicator();
+                          },
+                          errorBuilder: (context, error, stackTrace) {
+                            return Icon(Icons.error);
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: Image.network(
-                      '',
-                      fit: BoxFit.cover,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return CircularProgressIndicator();
-                      },
-                      errorBuilder: (context, error, stackTrace) {
-                        return Icon(Icons.error);
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                );
+              },
             );
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => AddMenu(),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
