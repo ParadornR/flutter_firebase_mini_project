36be4342071e27a64b1e607db427ffda5a469621
imxintro5 <paradorn.rinrit63@gmail.com>
2025-03-26 16:03:24 +0700

    [mix]"add inkwell push to detail.dart"

--- lib/app/ui/home.dart
+++ lib/app/ui/home.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_mini_project/app/ui/add_menu.dart';
+import 'package:flutter_firebase_mini_project/app/ui/detail.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
@@ -30,58 +31,73 @@ class Home extends StatelessWidget {
                 separatorBuilder: (context, index) => SizedBox(height: 8),
                 itemBuilder: (context, index) {
                   final data = docs[index].data() as Map<String, dynamic>;
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    height: 220,
-                    width: MediaQuery.of(context).size.width,
-                    clipBehavior: Clip.antiAlias,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data["chef"],
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                                Text(
-                                  data["menu"],
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder:
+                              (BuildContext context) => Detail(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      height: 220,
+                      width: MediaQuery.of(context).size.width,
+                      clipBehavior: Clip.antiAlias,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data["chef"],
+                                    style: TextStyle(fontSize: 16),
                                   ),
-                                ),
-                                Text(
-                                  data["ingredients"],
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ],
+                                  Text(
+                                    data["menu"],
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    data["ingredients"],
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          child: Image.network(
-                            data["url"],
-                            fit: BoxFit.cover,
-                            height: double.infinity,
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress == null) return child;
-                              return CircularProgressIndicator();
-                            },
-                            errorBuilder: (context, error, stackTrace) {
-                              return Icon(Icons.error);
-                            },
+                          Expanded(
+                            child: Image.network(
+                              data["url"],
+                              fit: BoxFit.cover,
+                              height: double.infinity,
+                              loadingBuilder: (
+                                context,
+                                child,
+                                loadingProgress,
+                              ) {
+                                if (loadingProgress == null) return child;
+                                return CircularProgressIndicator();
+                              },
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(Icons.error);
+                              },
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   );
                 },
