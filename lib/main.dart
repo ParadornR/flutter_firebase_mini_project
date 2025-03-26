b86126c7c5fa08260a9297ef6933d078ef343aa9
imxintro5 <paradorn.rinrit63@gmail.com>
2025-03-26 15:45:30 +0700

    [mix]"don't show debug banner"

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData(), home: Home());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: Home(),
+    );
   }
 }
